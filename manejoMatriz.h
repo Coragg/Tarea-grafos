c4bc1bc55376d973e08225241b3fd8bae30d6dbe
victor camero <v_e_c_g@outlook.com>
2022-05-09 18:46:26 -0400

    metodo darMemoriaMatriz, se hizo mas comprensible lo que hace con el cambio de nombre en las variables

--- manejoMatriz.h
+++ manejoMatriz.h
@@ -3,61 +3,73 @@
 #define inf 3000000
 
 typedef int BOOL;
+/*
 
-int **DarMemoriaMatriz(int m, int n) {
-    int **aux, i, j;
+*/
+int **DarMemoriaMatriz(int cantidadDeDatosEnFila, int cantidadDeDatosEnColumna) {
+    int **matriz, fila, columna;
 
-    aux = (int **)malloc(m*sizeof(int *));
-    for(i=0;i<m;i++)
-        aux[i] = (int *)malloc(n*sizeof(int));
-    for(i=0;i<m;i++)
-        for(j=0;j<n;j++)
-            aux[i][j] = 0;
-    return aux;
+    matriz = (int **)malloc(cantidadDeDatosEnFila*sizeof(int *));
+    for(fila = 0; fila < cantidadDeDatosEnFila; fila++)
+        matriz[fila] = (int *)malloc(cantidadDeDatosEnColumna*sizeof(int));
+    for(fila = 0; fila < cantidadDeDatosEnFila; fila++)
+        for(columna = 0; columna < cantidadDeDatosEnColumna; columna++)
+            matriz[fila][columna] = 0;
+    return matriz;
 }
-int *DarMemoriaArreglo(int n) {
-    int *aux, i;
+/*
 
-    aux = (int *)malloc(n*sizeof(int));
-    for(i=0;i<n;i++)
-        aux[i] = FALSE;
-    return aux;
+*/
+int *DarMemoriaArreglo(int numeroDeFilas) {
+    int *unidimencionalidadDeLaMatriz, posicionDeLaFila;
+
+    unidimencionalidadDeLaMatriz = (int *)malloc(numeroDeFilas*sizeof(int));
+    for(posicionDeLaFila = 0; posicionDeLaFila < numeroDeFilas; posicionDeLaFila++)
+        unidimencionalidadDeLaMatriz[posicionDeLaFila] = FALSE;
+    return unidimencionalidadDeLaMatriz;
 }
 
-int numeroDeNodosSinVisitar(int *Visitado, int n) {
-    int i, cont;
+/*
 
-    i = 0;
-    cont = 0;
+*/
+int numeroDeNodosSinVisitar(int *Visitado, int CantidadDeDatosEnFilaNoVistos) {
+    int posicion, contador;
 
-    while (i < n) {
-        if (Visitado[i] == FALSE) {
-            cont++;
+    posicion = 0;
+    contador = 0;
+
+    while (posicion < CantidadDeDatosEnFilaNoVistos) {
+        if (Visitado[posicion] == FALSE) {
+            contador++;
         }
-        i++;
+        posicion++;
     }
-   return cont;
+   return contador;
 }
+
+/*
+
+*/
 int nodoMinimoCostoSinVisitar(int *Visitado, int *Costo, int n) {
-    int i, nodo, minimo;
+    int posicionEnLaFila, nodo, minimo;
     BOOL esElPrimero;
 
     esElPrimero = TRUE;
-    i = 0;
-    while(i < n) {
-        if (Visitado[i] == FALSE) {
+    posicionEnLaFila = 0;
+    while(posicionEnLaFila < n) {
+        if (Visitado[posicionEnLaFila] == FALSE) {
             if (esElPrimero) {
-                minimo = Costo[i];
-                nodo = i;
+                minimo = Costo[posicionEnLaFila];
+                nodo = posicionEnLaFila;
                 esElPrimero = FALSE;
             } else {
-                if (Costo[i] < minimo) {
-                    minimo = Costo[i];
-                    nodo = i;
+                if (Costo[posicionEnLaFila] < minimo) {
+                    minimo = Costo[posicionEnLaFila];
+                    nodo = posicionEnLaFila;
                 }
             }
         }
-        i++;
+        posicionEnLaFila++;
     }
     return nodo;
 }
