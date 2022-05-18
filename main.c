8ef43683396916ec62286f50276e10f96285a036
victor camero <v_e_c_g@outlook.com>
2022-05-17 21:26:13 -0400

    codigo ordenado con prettier

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 #include <string.h>
 #include "manejoMatriz.h"
 
-//constantes
+// constantes
 #define name 45
-BOOL *Visitado = NULL;
 BOOL *Visitado;
+BOOL *Visitado = NULL;
 int **Grafo = NULL;
 int n;
 int m;
@@ -15,13 +15,15 @@ int D;
 int *Costo;
 int *NodoAnterior;
 
-void InicializaVisitados() {
+void InicializaVisitados()
+{
     int posicionFila;
-    for(posicionFila = 0; posicionFila < n; posicionFila++)
+    for (posicionFila = 0; posicionFila < n; posicionFila++)
         Visitado[posicionFila] = FALSE;
 }
 
-void imprimirCamino(int *NodoAnterior, int *Costo, int n, int v0){
+void imprimirCamino(int *NodoAnterior, int *Costo, int n, int v0)
+{
     int *Camino, i, columna, nodo, nodo1, nodo2;
     Camino = DarMemoriaArreglo(n);
 
@@ -29,31 +31,30 @@ void imprimirCamino(int *NodoAnterior, int *Costo, int n, int v0){
     Camino[columna] = D;
     columna++;
     nodo = NodoAnterior[D];
-    while(nodo != I) {
+    while (nodo != I)
+    {
         Camino[columna] = nodo;
         columna++;
         nodo = NodoAnterior[nodo];
     }
     Camino[columna] = nodo;
 
-    //printf("\n\t%2d -> %2d ($%3d): ", I, D, Costo[D]);
-
-    for (i = 0; i < columna; i++){
+    for (i = 0; i < columna; i++)
+    {
         nodo1 = Camino[i];
-        nodo2 = Camino[i+1];
+        nodo2 = Camino[i + 1];
         Grafo[nodo2][nodo1] = 0;
-
     }
 
-    while (columna >= 0) {
+    while (columna >= 0)
+    {
         printf("%3d", Camino[columna]);
         columna--;
     }
 }
 
-
-
-void niIdeaQueHaces(int *NodoAnterior, int *Costo, int numeroDeNodo, int v0){
+void niIdeaQueHaces(int *NodoAnterior, int *Costo, int numeroDeNodo, int v0)
+{
     int *Camino, i, columna, nodo, nodo1, nodo2;
 
     Camino = DarMemoriaArreglo(numeroDeNodo);
@@ -62,79 +63,85 @@ void niIdeaQueHaces(int *NodoAnterior, int *Costo, int numeroDeNodo, int v0){
     Camino[columna] = D;
     columna++;
     nodo = NodoAnterior[D];
-    while(nodo != I) {
+    while (nodo != I)
+    {
         Camino[columna] = nodo;
         columna++;
         nodo = NodoAnterior[nodo];
     }
     Camino[columna] = nodo;
 
-    for (i = 0; i < columna; i++){
+    for (i = 0; i < columna; i++)
+    {
         nodo1 = Camino[i];
-        nodo2 = Camino[i+1];
+        nodo2 = Camino[i + 1];
         Grafo[nodo2][nodo1] = 0;
-
     }
-    while (columna >= 0) {
+    while (columna >= 0)
+    {
         columna--;
     }
 }
 
-
-void DIJKSTRA(int **Grafo, int v0) {
-    int i, j, w=v0;
+void DIJKSTRA(int **Grafo, int v0)
+{
+    int i, j, w = v0;
 
     Visitado = DarMemoriaArreglo(n);
     Costo = DarMemoriaArreglo(n);
     NodoAnterior = DarMemoriaArreglo(n);
 
     Visitado[v0] = TRUE;
-    for(i=0;i<n;i++) {
+    for (i = 0; i < n; i++)
+    {
         if (Grafo[v0][i] == 0)
             Costo[i] = inf;
         else
             Costo[i] = Grafo[v0][i];
         NodoAnterior[i] = v0;
     }
-    while (numeroDeNodosSinVisitar(Visitado, n)>1) {
+    while (numeroDeNodosSinVisitar(Visitado, n) > 1)
+    {
         w = nodoMinimoCostoSinVisitar(Visitado, Costo, n);
         Visitado[w] = TRUE;
-        for(j=0;j<n;j++)
+        for (j = 0; j < n; j++)
         {
-            if ((Grafo[w][j] != 0) && (!Visitado[j])) {
-                if (Costo[w] + Grafo[w][j] < Costo[j]) {
+            if ((Grafo[w][j] != 0) && (!Visitado[j]))
+            {
+                if (Costo[w] + Grafo[w][j] < Costo[j])
+                {
                     NodoAnterior[j] = w;
                     Costo[j] = Costo[w] + Grafo[w][j];
                 }
             }
         }
     }
-
 }
 
-
-
-void LeeGrafo(char nomArchivo[20]) {
+void LeeGrafo(char nomArchivo[20])
+{
     FILE *pArchivo;
     int i, u, v, c;
 
     pArchivo = fopen(nomArchivo, "r");
-    if (pArchivo == NULL) {
+    if (pArchivo == NULL)
+    {
         printf("\n\n\tERROR: Archivo no encontrado.");
         exit(1);
     }
 
     fscanf(pArchivo, "%d", &n);
-    Grafo = DarMemoriaMatriz(n,n);
-    fscanf(pArchivo, "%d",& m);
-    fscanf(pArchivo, "%d",& I); 
-    fscanf(pArchivo, "%d",& D); 
-    i= 1;
+    Grafo = DarMemoriaMatriz(n, n);
+    fscanf(pArchivo, "%d", &m);
+    fscanf(pArchivo, "%d", &I);
+    fscanf(pArchivo, "%d", &D);
+    i = 1;
 
-    while (i<=m) {
-        fscanf(pArchivo, "%d",&u);// u es igual a la fila
-        fscanf(pArchivo, "%d",&v); // v es la columna
-        fscanf(pArchivo, "%d",&c);
+    while (i <= m)
+    {
+        fscanf(pArchivo, "%d", &u); // u es igual a la fila
+        fscanf(pArchivo, "%d", &v); // v es la columna
+        fscanf(pArchivo, "%d", &c);
         Grafo[u][v] = c;
         i++;
     }
@@ -143,12 +150,12 @@ void LeeGrafo(char nomArchivo[20]) {
     fclose(pArchivo);
 }
 
-
-
-void revisarCamino(){
+void revisarCamino()
+{
     int i;
     int camino_casi_corto;
-    for (i = 0; i <= 2; i++){
+    for (i = 0; i <= 2; i++)
+    {
         DIJKSTRA(Grafo, I);
         camino_casi_corto = Costo[D];
         niIdeaQueHaces(NodoAnterior, camino_casi_corto, n, I);
@@ -159,7 +166,8 @@ void revisarCamino(){
     imprimirCamino(NodoAnterior, camino_casi_corto, n, I);
 }
 
-void comprobarCamino() {
+void comprobarCamino()
+{
     int x, y, z;
     DIJKSTRA(Grafo, I);
     Costo[D];
@@ -169,66 +177,67 @@ void comprobarCamino() {
     Costo[D];
     niIdeaQueHaces(NodoAnterior, Costo, n, I);
     y = Costo[D];
-    if(x != y) {
+    if (x != y)
+    {
         printf("\nDistancia total: %d", y);
         printf("\nCamino casi mas corto:");
         imprimirCamino(NodoAnterior, y, n, I);
-    } else {
+    }
+    else
+    {
         // hay que ver como hacemos el bucle
-        //printf("\nNecesitamos un bucle");
+        // printf("\nNecesitamos un bucle");
         z = y;
-        while(y == z && z <= 5) {
+        while (y == z && z <= 5)
+        {
             DIJKSTRA(Grafo, I);
             Costo[D];
             niIdeaQueHaces(NodoAnterior, Costo, n, I);
             z = Costo[D];
-        }    
+        }
         printf("\nDistancia total: %d", z);
         printf("\nCamino casi mas corto:");
         imprimirCamino(NodoAnterior, z, n, I);
     }
-        
 }
 
-void caminoCasiMasCorto() {
+void caminoCasiMasCorto()
+{
     int x, y;
     DIJKSTRA(Grafo, I);
     Costo[D];
     niIdeaQueHaces(NodoAnterior, Costo[D], n, I);
     x = Costo[D];
     y = x;
-    while(y == x && y <= 5){
+    while (y == x && y <= 5)
+    {
         DIJKSTRA(Grafo, I);
         Costo[D];
-        niIdeaQueHaces(NodoAnterior, Costo[D], n, I); 
-        y = Costo[D]; 
+        niIdeaQueHaces(NodoAnterior, Costo[D], n, I);
+        y = Costo[D];
     }
     printf("Distancia total: %d", y);
     printf("\nCamino casi mas corto:");
     imprimirCamino(NodoAnterior, y, n, I);
 }
 
-
-
-void preProcesamiento() {
+void preProcesamiento()
+{
     char nomArchivo[20];
     int v0;
     int i;
 
     printf("Ingrese nombre del archivo: ");
     gets(nomArchivo);
-    LeeGrafo(nomArchivo); 
+    LeeGrafo(nomArchivo);
 
     InicializaVisitados();
 
-
     caminoCasiMasCorto();
-
 }
 
-
-
-int main() {
+int main()
+{
     preProcesamiento();
     printf("\n\n");
     return 0;
--- manejoMatriz.h
+++ manejoMatriz.h
@@ -6,25 +6,27 @@ typedef int BOOL;
 /*
 
 */
-int **DarMemoriaMatriz(int cantidadDeDatosEnFila, int cantidadDeDatosEnColumna) {
+int **DarMemoriaMatriz(int cantidadDeDatosEnFila, int cantidadDeDatosEnColumna)
+{
     int **matriz, fila, columna;
 
-    matriz = (int **)malloc(cantidadDeDatosEnFila*sizeof(int *));
-    for(fila = 0; fila < cantidadDeDatosEnFila; fila++)
-        matriz[fila] = (int *)malloc(cantidadDeDatosEnColumna*sizeof(int));
-    for(fila = 0; fila < cantidadDeDatosEnFila; fila++)
-        for(columna = 0; columna < cantidadDeDatosEnColumna; columna++)
+    matriz = (int **)malloc(cantidadDeDatosEnFila * sizeof(int *));
+    for (fila = 0; fila < cantidadDeDatosEnFila; fila++)
+        matriz[fila] = (int *)malloc(cantidadDeDatosEnColumna * sizeof(int));
+    for (fila = 0; fila < cantidadDeDatosEnFila; fila++)
+        for (columna = 0; columna < cantidadDeDatosEnColumna; columna++)
             matriz[fila][columna] = 0;
     return matriz;
 }
 /*
 
 */
-int *DarMemoriaArreglo(int numeroDeFilas) {
+int *DarMemoriaArreglo(int numeroDeFilas)
+{
     int *unidimencionalidadDeLaMatriz, posicionDeLaFila;
 
-    unidimencionalidadDeLaMatriz = (int *)malloc(numeroDeFilas*sizeof(int));
-    for(posicionDeLaFila = 0; posicionDeLaFila < numeroDeFilas; posicionDeLaFila++)
+    unidimencionalidadDeLaMatriz = (int *)malloc(numeroDeFilas * sizeof(int));
+    for (posicionDeLaFila = 0; posicionDeLaFila < numeroDeFilas; posicionDeLaFila++)
         unidimencionalidadDeLaMatriz[posicionDeLaFila] = FALSE;
     return unidimencionalidadDeLaMatriz;
 }
@@ -32,38 +34,48 @@ int *DarMemoriaArreglo(int numeroDeFilas) {
 /*
 
 */
-int numeroDeNodosSinVisitar(int *Visitado, int CantidadDeDatosEnFilaNoVistos) {
+int numeroDeNodosSinVisitar(int *Visitado, int CantidadDeDatosEnFilaNoVistos)
+{
     int posicion, contador;
 
     posicion = 0;
     contador = 0;
 
-    while (posicion < CantidadDeDatosEnFilaNoVistos) {
-        if (Visitado[posicion] == FALSE) {
+    while (posicion < CantidadDeDatosEnFilaNoVistos)
+    {
+        if (Visitado[posicion] == FALSE)
+        {
             contador++;
         }
         posicion++;
     }
-   return contador;
+    return contador;
 }
 
 /*
 
 */
-int nodoMinimoCostoSinVisitar(int *Visitado, int *Costo, int n) {
+int nodoMinimoCostoSinVisitar(int *Visitado, int *Costo, int n)
+{
     int posicionEnLaFila, nodo, minimo;
     BOOL esElPrimero;
 
     esElPrimero = TRUE;
     posicionEnLaFila = 0;
-    while(posicionEnLaFila < n) {
-        if (Visitado[posicionEnLaFila] == FALSE) {
-            if (esElPrimero) {
+    while (posicionEnLaFila < n)
+    {
+        if (Visitado[posicionEnLaFila] == FALSE)
+        {
+            if (esElPrimero)
+            {
                 minimo = Costo[posicionEnLaFila];
                 nodo = posicionEnLaFila;
                 esElPrimero = FALSE;
-            } else {
-                if (Costo[posicionEnLaFila] < minimo) {
+            }
+            else
+            {
+                if (Costo[posicionEnLaFila] < minimo)
+                {
                     minimo = Costo[posicionEnLaFila];
                     nodo = posicionEnLaFila;
                 }
@@ -73,4 +85,3 @@ int nodoMinimoCostoSinVisitar(int *Visitado, int *Costo, int n) {
     }
     return nodo;
 }
-
